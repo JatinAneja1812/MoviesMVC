6bfe11226e8f3c141f487e04a0114bb0ada53345
W9359412 <79636802+W9359412@users.noreply.github.com>
2021-10-29 15:16:15 +0100

    Add project files.

---
+++ Movies.Web/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Movies.Web.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        public int PersonId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        public int NationalityId { get; set; }
+
+        public Nationality Nationality { get; set; }
+
+    }
+}
